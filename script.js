1944cc24704bd367293eacf91dbe88a15038e77e
MaheshJV9130 <maheshvispute082@gmail.com>
2025-02-28 20:53:32 +0530

    Dog img api added and new stylling

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let url ="https://dogapi.dog/api/v1/facts";
+let url = "https://dogapi.dog/api/v1/facts";
 const facts = document.getElementById("facts");
-const generate =document.getElementById("generate");
+const generate = document.getElementById("generate");
+let dogImgurl = "https://dog.ceo/api/breeds/image/random";
+let dogImg = document.getElementById("dogImg");
+getData();
+getDogImg();
 
-async function getData(){
-
-    let response = await fetch(url)
-    let jsonForm = await response.json();
-    let txtFact = String(jsonForm.facts)
-    console.log(txtFact);
-    facts.innerHTML = txtFact;
-    
+async function getData() {
+  let response = await fetch(url);
+  let jsonForm = await response.json();
+  let txtFact = String(jsonForm.facts);
+  console.log(txtFact);
+  facts.innerHTML = txtFact;
 }
 
-generate.addEventListener('click',()=>{
-    getData()
+
+async function getDogImg() {
+  let response = await fetch(dogImgurl);
+  let img = await response.json();
+  let imgUrl = img.message;
+  dogImg.src = imgUrl
+}
+
+generate.addEventListener("click", () => {
+  getData();
+  getDogImg();
 });
